76a24744648a33f104015a25c2c3cd6526d12775
fff <assa.x0633.su@mail.ru>
2021-03-27 20:22:51 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static System.Math;
+using static System.Console;
+
+namespace pract6_1zadanie__5_
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                var rand = new Random();
+                var f = new FileStream("text.txt", FileMode.Create, FileAccess.ReadWrite);
+                var n = new byte[10];
+                WriteLine("\n\t\t\tПервый поток");
+                for (int i = 0; i < 10; i++)
+                {
+                    n[i] = (byte)(rand.Next(0, 255));
+                    WriteLine(n[i]);
+                }
+                WriteLine("\n\n\t\t\tВторой поток");
+                f.Write(n, 0, 10);
+                f.Seek(0, SeekOrigin.Begin);
+                var g = new byte[f.Length];
+                double k = 1;
+                for (int i = 0; i < f.Length; i++)
+                {
+                    g[i] = (byte)f.ReadByte();
+                    k *= g[i];
+                    WriteLine(g[i]);
+                }
+                WriteLine($"\n\nПроизведение чисел в файле равно: {k}");
+                f.Close();
+            }
+            catch 
+            {
+                WriteLine("Произошла ошибочка");
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("pract6_1zadanie(№5)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("pract6_1zadanie(№5)")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("094cd7f0-16aa-4e0e-bfff-06faa991dbd3")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
