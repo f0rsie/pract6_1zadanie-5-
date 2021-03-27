964f61a5242dc9beada525173d846d904e6357ae
fff <assa.x0633.su@mail.ru>
2021-03-27 22:31:31 +0300

    ..

--- Program.cs
+++ Program.cs
@@ -18,13 +18,13 @@ namespace pract6_1zadanie__5_
                 var rand = new Random();
                 var f = new FileStream("text.txt", FileMode.Create, FileAccess.ReadWrite);
                 var n = new byte[10];
-                WriteLine("\n\t\t\tПервый поток");
+                WriteLine("\n\t\t\tЗапись в файл");
                 for (int i = 0; i < 10; i++)
                 {
                     n[i] = (byte)(rand.Next(0, 255));
                     WriteLine(n[i]);
                 }
-                WriteLine("\n\n\t\t\tВторой поток");
+                WriteLine("\n\n\t\t\tЧтение из файла");
                 f.Write(n, 0, 10);
                 f.Seek(0, SeekOrigin.Begin);
                 var g = new byte[f.Length];
@@ -37,11 +37,13 @@ namespace pract6_1zadanie__5_
                 }
                 WriteLine($"\n\nПроизведение чисел в файле равно: {k}");
                 f.Close();
+                ReadKey();
             }
             catch 
             {
                 WriteLine("Произошла ошибочка");
             }
+            ReadKey();
         }
     }
 }
